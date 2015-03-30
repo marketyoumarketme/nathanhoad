fc9a28f6910ce262518e2c4b06a8367cc91dd608
marketyoumarketme <jack@marketyoumarketme.com>
2015-03-30 21:52:09 +1000

    convert to entrepreneurgames

--- models.rb
+++ models.rb
@@ -1,13 +1,13 @@
 class Post < Maneki
   path 'posts'
-  
-  
+
+
   # Grab a handful of posts
   def self.index (count = 7)
     all.sort[0...count]
   end
-  
-  
+
+
   # Archive posts
   def self.archive
     years_months_posts = all.inject({}) do |archive, post|
@@ -18,25 +18,25 @@ class Post < Maneki
     end
     years_months_posts.sort.reverse
   end
-  
+
   # Search for some posts
   def self.search (keywords)
     found = {}
     keywords.split(' ').each do |keyword|
-      posts = find :match => :any, :title => keyword, :body => keyword, :headers => { :tags => keyword }      
-      
+      posts = find :match => :any, :title => keyword, :body => keyword, :headers => { :tags => keyword }
+
       # First lot of results is our base
       if found.empty?
         found = posts
       end
-      
+
       # Then remove anything that doesn't match all keywords
       found = found.select { |post| posts.include? post }
     end
     found.sort
   end
-  
-  
+
+
   # Find posts that are tagged with a given tag
   def self.find_tagged_with (tag)
     posts = Post.find :headers => { :tags => tag }
@@ -59,8 +59,8 @@ class Post < Maneki
 
     related_posts
   end
-  
-  
+
+
   # This post's tags
   def tags
     t = @headers[:tags]
@@ -68,20 +68,20 @@ class Post < Maneki
 
     t
   end
-  
-  
+
+
   # When a post was published
   def published_at
     Chronic.parse(@headers[:published]) if @headers[:published]
   end
-  
-  
+
+
   # Some posts are just links
   def link
     @headers[:link]
   end
-  
-  
+
+
   # Check if this post is valid
   def valid?
     return false if @headers.nil?
@@ -89,8 +89,8 @@ class Post < Maneki
     return false if tags.nil? or tags.empty?
     true
   end
-  
-  
+
+
   # Sort by publish date in reverse order
   def <=> (rhs)
     rhs.published_at <=> published_at
